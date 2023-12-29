192cd96383d17f1842e94818167883cab7038c11
Drew Naumann <naumannadn@gmail.com>
2023-12-28 17:08:04 -0700

    removed private ip lol

--- server.js
+++ server.js
@@ -77,6 +77,4 @@ io.on("connection", (socket) => {
 
 const PORT = 3000 || process.env.PORT;
 
-server.listen(PORT, "192.168.86.28", () =>
-  console.log(`Server running on ${PORT}`)
-);
+server.listen(PORT, () => console.log(`Server running on ${PORT}`));
