5dfb61d3a34882aa3f675dc83a5a3f32131a428a
Drew Naumann <naumannadn@gmail.com>
2023-12-28 17:06:41 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,82 @@
+const path = require("path");
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+const formatMessage = require("./utils/messages");
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers,
+} = require("./utils/users");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+const assistantName = "ChatCord Assistant";
+
+// Run when client wants to
+io.on("connection", (socket) => {
+  socket.on("joinRoom", ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
+    socket.join(user.room);
+
+    // Welcome a new user
+
+    socket.emit(
+      "message",
+      formatMessage(assistantName, "Welcome to ChatCord!")
+    );
+
+    // Broadcast when a user connects
+
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        "message",
+        formatMessage(assistantName, `${user.username} has joined the chat`)
+      );
+
+    io.to(user.room).emit("roomUsers", {
+      room: user.room,
+      users: getRoomUsers(user.room),
+    });
+  });
+
+  // Listen for chat message
+
+  socket.on("chatMessage", (msg) => {
+    const user = getCurrentUser(socket.id);
+
+    io.to(user.room).emit("message", formatMessage(user.username, msg));
+  });
+
+  // Runs when the client disconnects
+
+  socket.on("disconnect", () => {
+    const user = userLeave(socket.id);
+
+    if (user) {
+      io.to(user.room).emit(
+        "message",
+        formatMessage(assistantName, `${user.username} has left the chat`)
+      );
+
+      // Send users and room info
+      io.to(user.room).emit("roomUsers", {
+        room: user.room,
+        users: getRoomUsers(user.room),
+      });
+    }
+  });
+});
+
+const PORT = 3000 || process.env.PORT;
+
+server.listen(PORT, "192.168.86.28", () =>
+  console.log(`Server running on ${PORT}`)
+);
